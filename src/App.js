c05efeef42245fbccee269c46cf616934da589ef
yarrrrrrik <60892661+yarrrrrrik@users.noreply.github.com>
2020-08-02 02:10:48 +0500

    Map tab developing.
    Cars sort works. Problem is that infoWindow opens twice.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReposTab from './components/repos-tab'
 import MapTab from './components/map-tab'
+import { YMaps,Map } from 'react-yandex-maps';
+
 // import {connect,useDispatch} from 'react-redux'
 // import {getRepos} from './redux/actions'
 
@@ -12,12 +14,23 @@ else return tab2
  // <ReposTab/>
 
 function App(props) {
-
+  const API_KEY = 'AIzaSyAm0qvYvetoYR18LjkGoD12UfSc9v1MIsA'
   return (
-    <div className="App">
-      <MapTab/>
+    <div style={{ width: "100vw", height: "100vh" }} className="App">
+
+      <MapTab
+        googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${API_KEY}`}
+        loadingElement={<div style={{ height: `100%` }} />}
+        containerElement={<div style={{ height: `400px` }} />}
+        mapElement={<div style={{ height: `100%` }} />}
+        />
+
     </div>
   );
 }
 
 export default App
+// googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyAm0qvYvetoYR18LjkGoD12UfSc9v1MIsA&v=3.exp&libraries=geometry,drawing,places"
+// loadingElement={<div style={{ height: `100%` }} />}
+// containerElement={<div style={{ height: `400px` }} />}
+// mapElement={<div style={{ height: `100%` }} />}
--- src/components/map-tab.js
+++ src/components/map-tab.js
@@ -1,12 +1,38 @@
-import React from 'react'
+import React,{useState} from 'react'
+// import { YMaps, Map, Placemark } from 'react-yandex-maps';
+import {GoogleMap, withScriptjs,withGoogleMap,Marker,InfoWindow} from "react-google-maps";
 
-function MapTab(props) {
+function Map() {
+  // const [variable,setVariable] = useState(false)
+  async function getCars() {
+    let request = await fetch('https://youdrive.today/info')
+    let carsData = await request.json()
+    let arr = []
+    let yekaterinburgCars = carsData.cars.filter((car, i) => {
+      if(car.latitude > 56.11 && car.latitude < 57.55 && car.longitude > 59.887 && car.longitude < 61.327){
+        return car
+      }
+    });
+    console.log(yekaterinburgCars)
 
-  return (
-    <div className="App">
-    бля у
-    </div>
-  );
+  }
+  getCars()
+  return(
+    <GoogleMap
+      defaultZoom={11}
+      defaultCenter={{ lat: 56.838272, lng: 60.607088 }}
+    >
+    <Marker position={{ lat: 56.838272, lng: 60.607088 }} onClick={() => {}}/>
+    </GoogleMap>
+  )
+
 }
+const MapTab = withScriptjs(withGoogleMap(Map));
+// {variable && (<InfoWindow position={{lat: 56.838272, lng: 60.607088 }}><h1>y</h1></InfoWindow>)}
 
+// export default GoogleApiWrapper({
+//   apiKey: ('AIzaSyAm0qvYvetoYR18LjkGoD12UfSc9v1MIsA')
+// })(MapTab)
 export default MapTab
+// AIzaSyAm0qvYvetoYR18LjkGoD12UfSc9v1MIsA
+// export default MapTab
